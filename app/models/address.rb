e8e062af69692465bb52573ce332874001561ca6
Dan Sudol <dansudol@yahoo.com>
2025-07-23 16:37:25 -0400

    validation refactor

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,5 @@
 class Address < ApplicationRecord
   belongs_to :investor
   validates :street, :state, :zip_code, presence: true
+  validates :zip_code, format: { with: /\A\d{5}\z/, message: "Zip code must be 5 digits" }
 end
